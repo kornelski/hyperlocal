8081cdda25b50e0364d5262cbc112bb98f570e79
Travis CI User <travis@example.org>
2020-01-10 13:59:02 +0000

    Update documentation

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["hyperlocal"] = [{text:"impl Freeze for <a class=\"struct\" href=\"hyperlocal/struct.UnixConnector.html\" title=\"struct hyperlocal::UnixConnector\">UnixConnector</a>",synthetic:true,types:["hyperlocal::client::UnixConnector"]},{text:"impl !Freeze for <a class=\"struct\" href=\"hyperlocal/struct.Uri.html\" title=\"struct hyperlocal::Uri\">Uri</a>",synthetic:true,types:["hyperlocal::uri::Uri"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ implementors/std/panic/trait.RefUnwindSafe.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["hyperlocal"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"hyperlocal/struct.UnixConnector.html\" title=\"struct hyperlocal::UnixConnector\">UnixConnector</a>",synthetic:true,types:["hyperlocal::client::UnixConnector"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"hyperlocal/struct.Uri.html\" title=\"struct hyperlocal::Uri\">Uri</a>",synthetic:true,types:["hyperlocal::uri::Uri"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
